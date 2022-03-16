6a978fb2ff69b4fe0f0c32a587238291b78b110c
chriszqyang <chriszqyang@tencent.com>
2022-03-16 18:20:36 +0800

    init commit

---
+++ app/controller/home.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const puppeteer = require("puppeteer");
+const Controller = require("egg").Controller;
+const fs = require("fs")
+
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+
+    ctx.set("content-type", "text/html");
+    await ctx.render("index.html", {
+      msg: "hi, egg",
+    });
+  }
+
+  async list_dir() {
+    const { ctx } = this;
+    const query = ctx.query;
+    console.log("parameter folder:" + query.name);
+    
+    const filesArray = fs.readdirSync(query.name);
+    ctx.body = [
+      {
+        files: filesArray,
+      },
+    ]; 
+  }
+
+  async test() {
+    const { ctx } = this;
+
+    const browser = await puppeteer.launch({
+      executablePath: "/opt/linux-818858/chrome-linux/chrome",
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.baidu.com");
+    const pageTitle = await page.title();
+    console.log("page title:" + pageTitle);
+    await browser.close();
+    ctx.body = [
+      {
+        page_title: pageTitle,
+      },
+    ];
+  }
+
+  async event() {
+    const { ctx } = this;
+    ctx.body = {
+      req: ctx.req.__SLS_EVENT__,
+      request: ctx.request.__SLS_EVENT__,
+    };
+  }
+
+  async getConfig() {
+    const { ctx } = this;
+    ctx.body = this.config;
+  }
+}
+
+module.exports = HomeController;
---
+++ app/utils/constants.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  ONE_SECOND: 1000,
+  PAGE_NUM: 4,
+  PLATFORM: 'TX'
+};
